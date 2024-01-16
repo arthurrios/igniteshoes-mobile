8f94a03e478d9dcf79ed2cbecf234ca04211d367
arthur <arthur.rios007@gmail.com>
2024-01-16 00:29:38 -0300

    setup: configs for android

--- src/libs/oneSignal.ts
+++ src/libs/oneSignal.ts
@@ -2,15 +2,15 @@ import { OneSignal } from 'react-native-onesignal';
 import { Platform } from 'react-native';
 
 function oneSignalInitialize() {
-  const {
-    EXPO_PUBLIC_ONE_SIGNAL_APP_ID_ANDROID,
-    EXPO_PUBLIC_ONE_SIGNAL_APP_ID_IOS,
-  } = process.env;
+  // const {
+  //   EXPO_PUBLIC_ONE_SIGNAL_APP_ID_ANDROID,
+  //   EXPO_PUBLIC_ONE_SIGNAL_APP_ID_IOS,
+  // } = process.env;
 
   const oneSignalAppId =
     Platform.OS === 'ios'
-      ? EXPO_PUBLIC_ONE_SIGNAL_APP_ID_IOS
-      : EXPO_PUBLIC_ONE_SIGNAL_APP_ID_ANDROID;
+      ? ""
+      : "e7569ea7-1579-4a97-a126-8a9c3dbbe119";
 
   if (oneSignalAppId) {
     OneSignal.initialize(oneSignalAppId);
