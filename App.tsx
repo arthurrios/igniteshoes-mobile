b8c336bf5ca68adb2133f17d17249659d2e5c781
arthur <arthur.rios007@gmail.com>
2024-01-16 21:51:30 -0300

    chore: add cart count tag

--- App.tsx
+++ App.tsx
@@ -15,36 +15,32 @@ import { Loading } from './src/components/Loading'
 import { CartContextProvider } from './src/contexts/CartContext'
 import { useEffect } from 'react'
 import { oneSignalInitialize } from './src/libs/oneSignal'
-import { tagUserInfoCreate } from './src/notifications/notificationsTags'
-import { OneSignal } from 'react-native-onesignal'
-import { NotificationEventTypeMap } from 'react-native-onesignal/dist/models/NotificationEvents'
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
 
   useEffect(() => {
     oneSignalInitialize()
-    tagUserInfoCreate()
   }, [])
 
-  useEffect(() => {
-    const unsubscribe = OneSignal.Notifications.addEventListener(
-      'click',
-      (notificationReceivedEvent: NotificationEventTypeMap['click']) => {
-        const { actionId } = notificationReceivedEvent.result
+  // useEffect(() => {
+  //   const unsubscribe = OneSignal.Notifications.addEventListener(
+  //     'click',
+  //     (notificationReceivedEvent: NotificationEventTypeMap['click']) => {
+  //       const { actionId } = notificationReceivedEvent.result
 
-        switch (actionId) {
-          case '1':
-            return console.log('See all')
-          case '2':
-            return console.log('See request')
-          default:
-            return console.log('Action button was not clicked')
-        }
-      },
-    )
-    return () => unsubscribe
-  }, [])
+  //       switch (actionId) {
+  //         case '1':
+  //           return console.log('See all')
+  //         case '2':
+  //           return console.log('See request')
+  //         default:
+  //           return console.log('Action button was not clicked')
+  //       }
+  //     },
+  //   )
+  //   return () => unsubscribe
+  // }, [])
 
   return (
     <NativeBaseProvider theme={THEME}>
--- src/notifications/notificationsTags.ts
+++ src/notifications/notificationsTags.ts
@@ -1,8 +1,12 @@
-import { OneSignal } from "react-native-onesignal";
+import { OneSignal } from 'react-native-onesignal'
 
 export function tagUserInfoCreate() {
   OneSignal.User.addTags({
-    'user_name': 'Arthur',
-    'user_email': 'arthur.rios007@gmail.com',
-  });
+    user_name: 'Arthur',
+    user_email: 'arthur.rios007@gmail.com',
+  })
+}
+
+export function tagCartUpdate(itemsCount: string) {
+  OneSignal.User.addTag('cart_items_count', itemsCount)
 }
